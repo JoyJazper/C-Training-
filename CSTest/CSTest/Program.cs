cd8643b1c1a73b13433d5c9a91be2d5c9aa1af9f
JoyJazper <pjk096@gmail.com>
2017-12-14 00:16:14 +0530

    New VS Code installed.

---
+++ CSTest/CSTest/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CSTest {
+	class MainClass {
+		public static void Main (string[] args) {
+			Console.WriteLine ("Hello World!");
+		}
+	}
+}
